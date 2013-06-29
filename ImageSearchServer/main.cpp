08426c4e7ce4eaec2e246aeb225f26d93150b0d3
Jan Quequezana <jan.quequezana@ucsp.edu.pe>
2013-06-28 22:24:37 -0500

    Initial Commit

---
+++ ImageSearchServer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ImageSearchServer
+//
+//  Created by Jan Quequezana on 28/06/13.
+//  Copyright (c) 2013 Jan Quequezana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
